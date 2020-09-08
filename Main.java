1a9b4e8dcb106202b606f04a736c5596a8ec677a
saidov017 <58263437+saidov017@users.noreply.github.com>
2020-09-08 13:30:37 +0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,11 @@
+package Dog;
+
+public class Main {
+    public static void main (String[]args){
+        Dog d1 = new Dog("Rex",2);
+        Dog d2 = new Dog("Yaya",6);
+        Dog d3 = new Dog("Duffy",3);
+        d1.intoHumanAge(); d2.intoHumanAge(); d3.intoHumanAge();
+
+                                          }
+}
